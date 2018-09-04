7950a7d9ef4d28c16afb129b482e0c2bfad4b328
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2018-09-04 09:31:33 +0200

    use red color to indicate 'problems' in class diagrams

--- Util.hs
+++ Util.hs
@@ -1,6 +1,7 @@
 module Util where
 
 import Data.Char
+import Data.GraphViz
 import Data.GraphViz.Attributes.Complete as Dot
 import Data.GraphViz.Attributes.HTML as HTML
 import Data.Text.Lazy (pack)
@@ -12,7 +13,10 @@ firstUpper :: String -> String
 firstUpper (c:cs) = toUpper c : cs
 
 underlinedLabel :: String -> Dot.Attribute
-underlinedLabel s = Label (HtmlLabel (Text [HTML.Format HTML.Underline [Str (pack s)]]))
+underlinedLabel s = Label (HtmlLabel (Text [HTML.Format HTML.Underline [HTML.Str (pack s)]]))
 
 emptyArr :: ArrowType
 emptyArr = AType [(openMod, Normal)]
+
+redColor :: Dot.Attribute
+redColor = Dot.Color [toWColor Red]
--- cd2pic.hs
+++ cd2pic.hs
@@ -14,15 +14,15 @@ import Data.GraphViz.Attributes.Complete
 import System.Environment (getArgs)
 import System.FilePath (dropExtension)
 
-data Connection = Inheritance | Assoc AssociationType (Int, Maybe Int) (Int, Maybe Int)
+data Connection = Inheritance | Assoc AssociationType (Int, Maybe Int) (Int, Maybe Int) Bool
 
 connectionArrow :: Connection -> [Attribute]
 connectionArrow Inheritance = [arrowTo emptyArr]
-connectionArrow (Assoc Composition from to) =
+connectionArrow (Assoc Composition from to isRed) =
   case from of
-    (1, Just 1) -> arrow Composition ++ [HeadLabel (mult to)]
-    (0, Just 1) -> arrow Composition ++ [TailLabel (mult from), HeadLabel (mult to)]
-connectionArrow (Assoc a from to) = arrow a ++ [TailLabel (mult from), HeadLabel (mult to)]
+    (1, Just 1) -> arrow Composition ++ [HeadLabel (mult to)] ++ [redColor | isRed]
+    (0, Just 1) -> arrow Composition ++ [TailLabel (mult from), HeadLabel (mult to)] ++ [redColor | isRed]
+connectionArrow (Assoc a from to isRed) = arrow a ++ [TailLabel (mult from), HeadLabel (mult to)] ++ [redColor | isRed]
 
 arrow :: AssociationType -> [Attribute]
 arrow Association = [ArrowHead noArrow]
@@ -43,14 +43,29 @@ draw file format input = do
   let syntax = parser tokens
   -- putStrLn ("SYNTAX:\n\n" ++ show syntax ++ "\n")
   let (classes, associations) = syntax
-  let theNodes = map fst classes
+  let classNames = map fst classes
+  let theNodes = classNames
   let inhEdges = mapMaybe (\(from,mto) -> fmap (\to -> (fromJust (elemIndex from theNodes), fromJust (elemIndex to theNodes), Inheritance)) mto) classes
-  let assocEdges = map (\(a,_,m1,from,to,m2) -> (fromJust (elemIndex from theNodes), fromJust (elemIndex to theNodes), Assoc a m1 m2)) associations
+  let classesWithSubclasses = map (\name -> (name, subs [] name)) classNames
+        where
+          subs seen name
+            | name `elem` seen = []
+            | otherwise = name : concatMap (subs (name:seen) . fst) (filter ((== Just name) . snd) classes)
+  let assocsBothWays = concatMap (\(_,_,_,from,to,_) -> [(from,to), (to,from)]) associations
+  let assocEdges = map (\(a,_,m1,from,to,m2) -> (fromJust (elemIndex from theNodes), fromJust (elemIndex to theNodes), Assoc a m1 m2 (shouldBeRed from to classesWithSubclasses assocsBothWays))) associations
   let graph = mkGraph (zip [0..] theNodes) (inhEdges ++ assocEdges) :: Gr String Connection
   let dotGraph = graphToDot (nonClusteredParams { fmtNode = \(_,l) -> [toLabel l, shape BoxShape], fmtEdge = \(_,_,l) -> connectionArrow l }) graph
   quitWithoutGraphviz "Please install GraphViz executables from http://graphviz.org/ and put them on your PATH"
   output <- addExtension (runGraphviz dotGraph) format (dropExtension file)
   putStrLn $ "Output written to " ++ output
+
+shouldBeRed :: String -> String -> [(String, [String])] -> [(String, String)] -> Bool
+shouldBeRed a b classesWithSubclasses = any (\(a',b') ->
+                                               (a /= a' || b /= b')
+                                               && let { one = a' `isSubOf` a; two = b' `isSubOf` b }
+                                                  in (one && (two || b `isSubOf` b') || two && (one || a `isSubOf` a'))
+                                            )
+  where x `isSubOf` y = x `elem` fromJust (lookup y classesWithSubclasses)
 
 main :: IO ()
 main = do
