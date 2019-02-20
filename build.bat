6914a92478945f5ca62e4a3c6128d202d5c0bc33
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2019-02-20 20:38:30 +0100

    rename task executable

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@ happy -i Parser.y
 cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints cd2alloy.hs
 cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints instance2pic.hs
 cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints cd2pic.hs
-cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints random-stuff.hs
+cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints random-task.hs
 javac -cp alloy/Alloy-5.0.0.1.jar RunAlloy.java -d .
