7bf61a1988ab50cfa4dd1cd1b278397f191cb458
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2019-02-22 15:55:42 +0100

    Enable file printing on alloy failure
     - reference issue-19

--- Alloy.hs
+++ Alloy.hs
@@ -1,20 +1,21 @@
 module Alloy where
 
+import Control.Monad   (when)
 import Data.List       (intercalate)
 import Data.List.Split (splitOn)
-
+import System.Exit     (ExitCode (..))
 import System.FilePath (searchPathSeparator)
-import System.IO
+import System.IO       (hClose, hGetLine, hIsEOF, hPutStr)
 import System.Process
 
 getInstances :: Int -> String -> IO [String]
 getInstances maxInstances content = do
   let callAlloy = proc "java" ["-cp", '.' : searchPathSeparator : "alloy/Alloy-5.0.0.1.jar",
                                "alloy.RunAlloy", show maxInstances]
-  (Just hin, Just hout, _, _) <- createProcess callAlloy { std_out = CreatePipe, std_in = CreatePipe }
+  (Just hin, Just hout, _, ph) <- createProcess callAlloy { std_out = CreatePipe, std_in = CreatePipe }
   hPutStr hin content
   hClose hin
-  fmap (intercalate "\n") . drop 1 . splitOn [begin] <$> getWholeOutput hout
+  printContentOnError ph `seq` fmap (intercalate "\n") . drop 1 . splitOn [begin] <$> getWholeOutput hout
   where
     begin = "---INSTANCE---"
     getWholeOutput h = do
@@ -22,6 +23,9 @@ getInstances maxInstances content = do
       if eof
         then return []
         else (:) <$> hGetLine h <*> getWholeOutput h
+    printContentOnError ph = do
+      code <- waitForProcess ph
+      when (code /= ExitSuccess) $ putStrLn content
 
 existInstances :: String -> IO Bool
 existInstances = fmap (not . null) . getInstances 1
