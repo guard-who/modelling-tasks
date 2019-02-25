1b760d73ed6fe31c4ba6a2445d45d727a34a16b8
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2019-02-25 09:26:47 +0100

    have possibility of "instance of neither of them"

--- random-task.hs
+++ random-task.hs
@@ -34,41 +34,49 @@ getRandomTask config maxObjects output searchSpace maxInstances = do
   let medges2 = getFirstValid names mutations
   continueIf (isJust medges2) $ do
     mutations' <- shuffleM mutations
+    mutations'' <- shuffleM mutations
     let Just edges2 = medges2
         Just edges3 = getFirstValid names mutations'
+        Just edges4 = getFirstValid names mutations''
         cd2 = fromEdges names edges2
         cd3 = fromEdges names edges3
+        cd4 = fromEdges names edges4
         parts2 = case transform cd2 "2" "" of (p1, p2, p3, p4, _) -> (p1, p2, p3, p4)
         parts3 = case transform cd3 "3" "" of (p1, p2, p3, p4, _) -> (p1, p2, p3, p4)
+        parts2and3 = mergeParts parts2 parts3
+        parts4 = case transform cd4 "4" "" of (p1, p2, p3, p4, _) -> (p1, p2, p3, p4)
         cd2not3 = createRunCommand "cd2 and (not cd3)" (length names) maxObjects
         cd3not2 = createRunCommand "cd3 and (not cd2)" (length names) maxObjects
         cd2and3 = createRunCommand "cd2 and cd3" (length names) maxObjects
+        cdNot2not3 = createRunCommand "(not cd2) and (not cd3) and cd4" (length names) maxObjects
     continueIf (not $ null $ nonTargets (singleton TInheritance) $ edges2 ++ edges3) $ do
-      instances2not3 <- getInstancesOfMerged parts2 parts3 cd2not3
-      instances3not2 <- getInstancesOfMerged parts2 parts3 cd3not2
-      instances2and3 <- getInstancesOfMerged parts2 parts3 cd2and3
-      let takes = [ (take x, take y, take z)
+      instances2not3 <- Alloy.getInstances maxInstances (combineParts parts2and3 ++ cd2not3)
+      instances3not2 <- Alloy.getInstances maxInstances (combineParts parts2and3 ++ cd3not2)
+      instances2and3 <- Alloy.getInstances maxInstances (combineParts parts2and3 ++ cd2and3)
+      instancesNot2not3 <- Alloy.getInstances maxInstances (combineParts (mergeParts parts2and3 parts4) ++ cdNot2not3)
+      let takes = [ (take x, take y, take z, take u)
                   | x <- [0 .. min 3 (length instances2not3)]
                   , y <- [0 .. min 3 (length instances3not2)]
-                  , z <- [0 .. min 3 (length instances2and3)]
-                  , 5 == x + y + z ]
+                  , z <- [0 .. min 2 (length instances2and3)]
+                  , u <- [0 .. min 2 (length instancesNot2not3)]
+                  , 5 == x + y + z + u ]
       continueIf (not $ null takes) $ do
-        (take2not3, take3not2, take2and3) <- head <$> shuffleM takes
+        (take2not3, take3not2, take2and3, takeNot2not3) <- head <$> shuffleM takes
         shuffled2not3 <- take2not3 <$> shuffleM instances2not3
         shuffled3not2 <- take3not2 <$> shuffleM instances3not2
         shuffled2and3 <- take2and3 <$> shuffleM instances2and3
+        shuffledNot2not3 <- takeNot2not3 <$> shuffleM instancesNot2not3
         drawCdFromSyntax True cd2 (output ++ '-' : "2") Pdf
         drawCdFromSyntax True cd3 (output ++ '-' : "3") Pdf
         mapM_ (uncurry $ drawOd "2not3") $ zip [1 :: Integer ..] shuffled2not3
         mapM_ (uncurry $ drawOd "3not2") $ zip [1 :: Integer ..] shuffled3not2
         mapM_ (uncurry $ drawOd "2and3") $ zip [1 :: Integer ..] shuffled2and3
+        mapM_ (uncurry $ drawOd "not2not3") $ zip [1 :: Integer ..] shuffledNot2not3
   where
     continueIf True  m = m
     continueIf False _ = getRandomTask config maxObjects output searchSpace maxInstances
     drawOd x y insta   =
       drawOdFromInstance True insta (output ++ '-' : x ++ '-' : show y) Pdf
-    getInstancesOfMerged x y command =
-      Alloy.getInstances maxInstances (combineParts (mergeParts x y) ++ command)
     combineParts (p1, p2, p3, p4) = p1 ++ p2 ++ p3 ++ p4
 
 getFirstValid :: [String] -> [[DiagramEdge]] -> Maybe [DiagramEdge]
