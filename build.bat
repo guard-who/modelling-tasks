f7e692f2d0ec8cf13d5b18137c3c2d8feba972db
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2020-03-11 10:25:35 +0100

    for local building at my end

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
-alex -i legacy-app/Auxiliary/Lexer.x
-happy -i legacy-app/Auxiliary/Parser.y
+alex -i legacy-app/Alloy/CdOd/Auxiliary/Lexer.x
+happy -i legacy-app/Alloy/CdOd/Auxiliary/Parser.y
 cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints -isrc:legacy-app legacy-app/cd2alloy.hs
 cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints -isrc:legacy-app legacy-app/instance2pic.hs
 cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints -isrc:legacy-app legacy-app/cd2pic.hs
 cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints -isrc:app app/match-cd-od.hs
 cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints -isrc:app app/different-names.hs
 cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints -isrc:app app/repair-incorrect.hs
+cabal exec -- ghc -Wall -Wincomplete-uni-patterns -Wincomplete-record-updates -Widentities -Wredundant-constraints -isrc:app app/check-cds.hs
