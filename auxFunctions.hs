b730e496236d7796f8ac7a6011bd2d6ec0a65cca
André <46451466+Akureimo@users.noreply.github.com>
2020-04-15 14:22:38 +0200

    general stack update

---
+++ auxFunctions.hs
@@ -0,0 +1,20 @@
+module AuxFunctions where
+--Auxiliary Functions--Hilfsfunktionen--
+
+import qualified Data.Set as Set
+
+--extract from Triples
+extractTFirst :: (a,b,c) -> a
+extractTFirst (a,_,_) = a
+
+extractTSecond :: (a,b,c) -> b
+extractTSecond (_,b,_) = b
+
+extractTThird :: (a,b,c) -> c
+extractTThird (_,_,c) = c
+
+--work with Set
+getFirstElem :: Set.Set a -> a
+getFirstElem s = Set.elemAt 0 s
+
+--work with Lists
