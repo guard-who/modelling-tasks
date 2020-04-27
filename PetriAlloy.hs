7a598f77b0c222f3602f45d83d33c490ec8f5249
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2020-04-27 17:31:27 +0200

    unused Alloy variable

--- PetriAlloy.hs
+++ PetriAlloy.hs
@@ -69,7 +69,7 @@ pred maxWeight[n : Int]{
   all weight : Nodes.flow[Nodes] | weight =< n
 }
 
-pred showNets [ps : Places, ts : Transitions, t,n : Int] {
+pred showNets [ts : Transitions, t,n : Int] {
   #Places = #{places}
   #Transitions = #{transitions}
   t >= #{minTknsOverall}
