e1bd2502988e8149ebfc9132cd8bcc5354af7cef
André <46451466+Akureimo@users.noreply.github.com>
2020-04-27 17:12:15 +0200

    marcellussiegburg/petri-tasks#14 Contraints

--- Inputs.hs
+++ Inputs.hs
@@ -13,25 +13,34 @@ userInput = do
   putStr "Anzahl der Transitionen: "
   trns <- getLine
   let inp = defaultInput{ places = read pls, transitions = read trns }
-  if checkInput inp 
-  then do
+  let c = checkInput inp
+  if c == Nothing
+  then do  
     out <- runPParser inp
     case out of 
       Left merror -> print merror
       Right petri -> renderNet petri (graphLayout inp)
-  else 
-    print "invalid Input"
-    
-checkInput :: Input -> Bool
+  else
+    print $ c
+  
+checkInput :: Input -> Maybe String
 checkInput Input{places,transitions,atLeastActiv,minTknsOverall,maxTknsOverall,maxTknsPerPlace,
-                 minFlowOverall,maxFlowOverall,maxFlowPerEdge} = 
-  places > 0 && transitions > 0 && atLeastActiv >= 0 && minTknsOverall >= 0 && maxTknsPerPlace >= 0 &&
-  maxFlowPerEdge > 0 && minFlowOverall >= 0 &&
-  atLeastActiv <= transitions &&
-  maxTknsOverall <= places*maxTknsPerPlace &&
-  maxTknsOverall >= minTknsOverall &&
-  maxTknsPerPlace <= maxTknsOverall &&
-  maxFlowOverall >= minFlowOverall &&
-  maxFlowOverall >= maxFlowPerEdge 
+                 minFlowOverall,maxFlowOverall,maxFlowPerEdge}
+ | places <= 0         = Just "There must at least be 1 Place"
+ | transitions <= 0    = Just "There must at least be 1 Transition"
+ | atLeastActiv < 0    = Just "Least Active Transitions must be at least 0"
+ | minTknsOverall < 0  = Just "Tokens Overall must be at least 0"
+ | maxTknsPerPlace < 0 = Just "Tokens per Place must be at least 0"
+ | maxFlowPerEdge <= 0 = Just "Max Flow per Edge must be at least 1"
+ | minFlowOverall < 0  = Just "Overall Flow must be at least 0"
+ | atLeastActiv > transitions              = Just ("Least Active Transitions must be lower than "
+                                                ++"Transitions")
+ | maxTknsOverall > places*maxTknsPerPlace = Just "choose a lower Max Token Overall"
+ | maxTknsOverall < minTknsOverall         = Just "Min and Max Tokens Overall aren't fitting"
+ | maxTknsPerPlace > maxTknsOverall        = Just ("Tokens Per Place must be lower than Max Tokens " 
+                                                ++ "Overall")
+ | maxFlowOverall < minFlowOverall         = Just "Min and Max FLow Overall aren't fitting"
+ | maxFlowOverall < maxFlowPerEdge         = Just "Flow Per Edge must be lower than Max Flow Overall"
+ | otherwise   = Nothing 
   
   
