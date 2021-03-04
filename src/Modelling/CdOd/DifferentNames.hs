384ae9e2c804ab7728dc529b6be45c75c934b032
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2021-03-04 19:43:12 +0100

    clearify name matching task (#81)
     - close #81

--- src/Modelling/CdOd/DifferentNames.hs
+++ src/Modelling/CdOd/DifferentNames.hs
@@ -90,6 +90,10 @@ differentNamesTask task = do
 State your answer by giving a mapping of relationships in the CD to links in the OD.
 To state that "foo" in the CD corresponds to "bar" in the OD and "foofoo" in the CD corresponds to "baz" in the OD write it as
 [("foo", "bar"), ("foofoo", "baz")].|]
+  paragraph $ text
+    [i|Please note: Links are already grouped correctly, i.e. all links with the same name in the OD correspond to exactly one relationship in the CD.
+Furthermore, links are completely grouped, i.e. every link name in the OD corresponds to exactly one relationship name in the CD.
+Thus, every link name and every relationship name should occur exactly once in your mapping.|]
   paragraph $ text simplifiedInformation
   paragraph $ text directionsAdvice
   paragraph $ text hoveringInformation
