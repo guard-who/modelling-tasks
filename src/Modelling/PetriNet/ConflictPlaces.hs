c9d2ab7d9abb626b0c8cd35f20aed2e06c225b11
Janis Voigtländer <jvoigtlaender@users.noreply.github.com>
2022-03-08 14:29:51 +0100

    typos/formulations

--- src/Modelling/PetriNet/ConflictPlaces.hs
+++ src/Modelling/PetriNet/ConflictPlaces.hs
@@ -103,16 +103,16 @@ findConflictPlacesTask path task = do
             (fmap show)
             conflictInitialShow
       english [i| as answer would indicate that transitions #{t1} and #{t2} are in conflict under the initial marking
-and that places #{p1} and #{p2} are all the common places within the preconditions,
+and that places #{p1} and #{p2} are all the common places within the preconditions
 which do not have enough tokens in order to fire #{t1} and #{t2} concurrently. |]
       german [i| als Antwort würde bedeuten, dass Transitionen #{t1} und #{t2} unter der Startmarkierung in Konflikt stehen
 und dass die Stellen #{p1} und #{p2} alle gemeinsamen Stellen in den Vorbedingungen sind,
-die jeweils nicht ausreichenden Marken zum gleichzeitigen Feuern der Transitionen #{t1} und #{t2} haben. |]
+die jeweils nicht ausreichend Marken zum gleichzeitigen Feuern der Transitionen #{t1} und #{t2} haben. |]
     translate $ do
       english [i|The order of transitions within the pair does not matter here.
-Furthermore, the order of places within the lisiting of places being the sources of the coflict is irrelevant.|]
+The order of places within the lisiting of places being the sources of the conflict is irrelevant as well.|]
       german [i|Die Reihenfolge der Transitionen innerhalb des Paars spielt hierbei keine Rolle.
-Die Reihenfolge der Stellen innerhalb der Auflistung der den Konflikt auslösenden stellen spielt ebenso keine Rolle.|]
+Die Reihenfolge der Stellen innerhalb der Auflistung der den Konflikt auslösenden Stellen spielt ebenso keine Rolle.|]
   paragraph hoveringInformation
 
 conflictInitial :: ConflictPlaces
