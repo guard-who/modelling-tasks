20b66a845e2f9809c6f6aa8d2e61417277b04328
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-04-01 23:09:49 +0200

    Fixed spacing

--- src/AD_Datatype.hs
+++ src/AD_Datatype.hs
@@ -1,43 +1,43 @@
 module AD_Datatype (
-    ADConnection(..),
-    ADNodes(..),
-    UMLActivityDiagram(..)
+  ADConnection(..),
+  ADNodes(..),
+  UMLActivityDiagram(..)
 ) where 
 
 data ADConnection = ADConnection Integer Integer String deriving (Show, Eq)
 
 data ADNodes =
-    ActivityNode {
-        label :: Integer,
-        name :: String
-    }
-    | ObjectNode {
-        label :: Integer,
-        name :: String
-    } 
-    | DecisionNode {
-        label :: Integer
-    } 
-    | MergeNode {
-        label :: Integer
-    }
-    | ForkNode {
-        label :: Integer
-    }
-    | JoinNode {
-        label :: Integer
-    }
-    | ActivityEndNode {
-        label :: Integer
-    }
-    | FlowEndNode {
-        label :: Integer
-    } deriving (Show, Eq)
+  ActivityNode {
+    label :: Integer,
+    name :: String
+  }
+  | ObjectNode {
+      label :: Integer,
+      name :: String
+  } 
+  | DecisionNode {
+      label :: Integer
+  } 
+  | MergeNode {
+      label :: Integer
+  }
+  | ForkNode {
+      label :: Integer
+  }
+  | JoinNode {
+      label :: Integer
+  }
+  | ActivityEndNode {
+      label :: Integer
+  }
+  | FlowEndNode {
+      label :: Integer
+  } deriving (Show, Eq)
 
 
 data UMLActivityDiagram =
-    UMLActivityDiagram {
-      nodes :: [ADNodes],
-      connections :: [ADConnection],
-      startNodes :: [Integer]
-    } deriving (Show, Eq)
+  UMLActivityDiagram {
+    nodes :: [ADNodes],
+    connections :: [ADConnection],
+    startNodes :: [Integer]
+  } deriving (Show, Eq)
