8e294f285a7f1196e235e29755f9e1af14234539
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-04-06 18:57:46 +0200

    Very basic first version of plantuml-converter

---
+++ src/AD_PlantUMLConverter.hs
@@ -0,0 +1,49 @@
+module AD_PlantUMLConverter (
+  convertToPlantUML
+) where
+
+import AD_Datatype (
+  ADConnection(ADConnection),
+  ADNode(..),
+  UMLActivityDiagram(..),
+  )
+
+
+convertToPlantUML :: UMLActivityDiagram -> String
+convertToPlantUML diag@(UMLActivityDiagram ns _) = 
+    let start = head  $ filter isStartNode ns
+        body = convertNode (Just start) diag
+    in "@startuml\n" ++ body ++ "@enduml"
+
+convertNode :: Maybe ADNode -> UMLActivityDiagram -> String
+convertNode node diag =
+  case node of
+    Just ADActionNode {label = l} -> ":A;\n" ++ convertNode (adjNode diag l) diag
+    Just ADObjectNode {label = l} -> ":O]\n" ++ convertNode (adjNode diag l) diag
+    Just ADStartNode {label = l} -> "start\n" ++ convertNode (adjNode diag l) diag
+    Just ADActivityEndNode {label = l} -> "end\n" ++ convertNode (adjNode diag l) diag
+    Just ADFlowEndNode {label = l} -> "stop\n" ++ convertNode (adjNode diag l) diag
+    _ -> ""
+
+
+adjNode :: UMLActivityDiagram -> Int -> Maybe ADNode
+adjNode (UMLActivityDiagram ns conns) l = 
+  let next = map to $ filter isAdjToLabel conns
+  in if null next then Nothing
+     else let adjnodes = filter (hasLabel (head next)) ns
+          in if null adjnodes then Nothing
+             else Just (head adjnodes)
+  where isAdjToLabel conn = (from conn) == l
+
+hasLabel :: Int -> ADNode -> Bool
+hasLabel l node = (label node) == l
+
+from :: ADConnection -> Int 
+from (ADConnection l _ _) = l
+
+to :: ADConnection -> Int
+to (ADConnection _ l _) = l
+
+isStartNode :: ADNode -> Bool
+isStartNode ADStartNode {} = True
+isStartNode _ = False
