f3b46b421b297c6f50cc27bddfb542e87b196acd
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-04-16 20:40:35 +0200

    Added helper methods for graph operations

--- src/AD_Datatype.hs
+++ src/AD_Datatype.hs
@@ -1,7 +1,9 @@
 module AD_Datatype (
   ADConnection(..),
   ADNode(..),
-  UMLActivityDiagram(..)
+  UMLActivityDiagram(..),
+  getInitialNodes,
+  adjNodes
 ) where 
 
 data ADConnection = ADConnection Int Int String deriving (Show, Eq, Ord)
@@ -43,3 +45,25 @@ data UMLActivityDiagram =
     nodes :: [ADNode],
     connections :: [ADConnection]
   } deriving (Show, Eq)
+
+
+adjNodes :: ADNode -> UMLActivityDiagram -> [ADNode]
+adjNodes node (UMLActivityDiagram ns conns) = 
+  let adjLabel = map getTo $ filter (\c -> getFrom c == getLabel node) conns
+  in filter (\node' -> getLabel node' `elem` adjLabel) ns
+
+
+getInitialNodes :: UMLActivityDiagram -> [ADNode]
+getInitialNodes (UMLActivityDiagram ns _) = 
+  filter isInitialNode ns
+  where isInitialNode ADInitialNode {} = True
+        isInitialNode _ = False
+
+getFrom :: ADConnection -> Int 
+getFrom (ADConnection l _ _) = l
+
+getTo :: ADConnection -> Int
+getTo (ADConnection _ l _) = l
+
+getLabel :: ADNode -> Int
+getLabel = label
