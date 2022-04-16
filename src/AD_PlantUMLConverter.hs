d9ce7f2e9e7b2acf5cece88ba690d7ee41fdd6cd
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-04-16 20:41:08 +0200

    Refactored PlantUMLConverter code

--- src/AD_PlantUMLConverter.hs
+++ src/AD_PlantUMLConverter.hs
@@ -3,47 +3,32 @@ module AD_PlantUMLConverter (
 ) where
 
 import AD_Datatype (
-  ADConnection(ADConnection),
   ADNode(..),
   UMLActivityDiagram(..),
+  getInitialNodes,
+  adjNodes,
   )
 
 
 convertToPlantUML :: UMLActivityDiagram -> String
-convertToPlantUML diag@(UMLActivityDiagram ns _) = 
-    let start = head  $ filter isInitialNode ns
-        body = convertNode (Just start) diag
+convertToPlantUML diag = 
+    let start = getInitialNodes diag
+        body = convertNode start diag
     in "@startuml\n" ++ body ++ "@enduml"
 
-convertNode :: Maybe ADNode -> UMLActivityDiagram -> String
-convertNode node diag =
-  case node of
-    Just ADActionNode {label = l} -> ":A;\n" ++ convertNode (adjNode diag l) diag
-    Just ADObjectNode {label = l} -> ":O]\n" ++ convertNode (adjNode diag l) diag
-    Just ADInitialNode {label = l} -> "start\n" ++ convertNode (adjNode diag l) diag
-    Just ADActivityFinalNode {label = l} -> "end\n" ++ convertNode (adjNode diag l) diag
-    Just ADFlowFinalNode {label = l} -> "stop\n" ++ convertNode (adjNode diag l) diag
-    _ -> ""
+convertNode :: [ADNode] -> UMLActivityDiagram -> String
+convertNode queue diag = convertNode' queue diag []
 
-
-adjNode :: UMLActivityDiagram -> Int -> Maybe ADNode
-adjNode (UMLActivityDiagram ns conns) l = 
-  let next = map to $ filter isAdjToLabel conns
-  in if null next then Nothing
-     else let adjnodes = filter (hasLabel (head next)) ns
-          in if null adjnodes then Nothing
-             else Just (head adjnodes)
-  where isAdjToLabel conn = (from conn) == l
-
-hasLabel :: Int -> ADNode -> Bool
-hasLabel l node = (label node) == l
-
-from :: ADConnection -> Int 
-from (ADConnection l _ _) = l
-
-to :: ADConnection -> Int
-to (ADConnection _ l _) = l
-
-isInitialNode :: ADNode -> Bool
-isInitialNode ADInitialNode {} = True
-isInitialNode _ = False
+--Use BFS to traverse the graph
+convertNode' :: [ADNode] -> UMLActivityDiagram -> [ADNode] -> String
+convertNode' [] _ _ = ""
+convertNode' (current:queue) diag seen = 
+  let newQueue = filter (`notElem` seen) (queue ++ adjNodes current diag)
+      newSeen = seen ++ [current]
+  in case current of 
+        ADActionNode {} -> ":A;\n" ++ convertNode' newQueue diag newSeen
+        ADObjectNode {} -> ":O]\n" ++ convertNode' newQueue diag newSeen
+        ADInitialNode {} -> "start\n" ++ convertNode' newQueue diag newSeen
+        ADActivityFinalNode {} -> "end\n" ++ convertNode' newQueue diag newSeen
+        ADFlowFinalNode {} -> "stop\n" ++ convertNode' newQueue diag newSeen
+        _ -> ""
