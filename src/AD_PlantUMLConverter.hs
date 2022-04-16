3a1e21e736177816ec5ca4402307f4ecdd9342db
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-04-16 21:53:58 +0200

    Added support for repeat to plantuml converter

--- src/AD_PlantUMLConverter.hs
+++ src/AD_PlantUMLConverter.hs
@@ -23,6 +23,7 @@ convertNode :: [ADNode] -> UMLActivityDiagram -> String
 convertNode queue diag = convertNode' queue diag []
 
 --Use BFS to traverse the graph
+--TODO: Handling fork/if-structures (finding a fork/decision node -> case distinction -> find corresponding merge -> traverse all paths to it)
 convertNode' :: [ADNode] -> UMLActivityDiagram -> [ADNode] -> String
 convertNode' [] _ _ = ""
 convertNode' (current:queue) diag seen = 
@@ -34,4 +35,24 @@ convertNode' (current:queue) diag seen =
         ADInitialNode {} -> "start\n" ++ convertNode' newQueue diag newSeen
         ADActivityFinalNode {} -> "end\n" ++ convertNode' newQueue diag newSeen
         ADFlowFinalNode {} -> "stop\n" ++ convertNode' newQueue diag newSeen
+        ADMergeNode {} -> "repeat\n" ++ handleRepeat current convertNode' newQueue diag newSeen
         _ -> ""
+
+
+--Might be better to get repeat-structures directly from the alloy instance, but should work for now
+--Traverses path until a decision node pointing towards the merge node is found, which is the corresponding repeatEnd
+handleRepeat :: ADNode -> ([ADNode] -> UMLActivityDiagram -> [ADNode] -> String) -> [ADNode] -> UMLActivityDiagram -> [ADNode] -> String
+handleRepeat _ _ [] _ _ = ""
+handleRepeat mergenode callback (current:queue) diag seen =
+    let newQueue = filter (`notElem` seen) (queue ++ adjNodes current diag)
+        newSeen = seen ++ [current]
+    in case current of 
+        ADActionNode {name = s} -> [i|:#{s};\n|] ++ handleRepeat mergenode callback newQueue diag newSeen
+        ADObjectNode {name = s} -> [i|:#{s}]\n|] ++ handleRepeat mergenode callback newQueue diag newSeen
+        ADInitialNode {} -> "start\n" ++ handleRepeat mergenode callback newQueue diag newSeen
+        ADActivityFinalNode {} -> "end\n" ++ handleRepeat mergenode callback newQueue diag newSeen
+        ADFlowFinalNode {} -> "stop\n" ++ handleRepeat mergenode callback newQueue diag newSeen
+        ADMergeNode {} -> "repeat\n" ++ handleRepeat current (handleRepeat mergenode callback) newQueue diag newSeen
+        ADDecisionNode {} -> if mergenode `elem` adjNodes current diag then "repeat while ()\n" ++ callback newQueue diag newSeen
+                             else "" -- TODO
+        _ -> "" 
