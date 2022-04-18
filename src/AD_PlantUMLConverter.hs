dbc04c9d04bac7696aed9990b02e68c70580077d
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-04-18 21:53:30 +0200

    Added support for fork nodes to plantumlconverter

--- src/AD_PlantUMLConverter.hs
+++ src/AD_PlantUMLConverter.hs
@@ -57,6 +57,19 @@ handleDecision decision _ diag@(UMLActivityDiagram _ conns) seen =
   in (intercalate "else ([Y])\n" subStrings) ++ "endif\n" ++ convertNode' newQueue diag newSeen
 
 
+--Same as handleDecision except for some tokens, TODO: pass those as parameters
+handleFork :: ADNode -> [ADNode] -> UMLActivityDiagram -> [ADNode] -> String
+handleFork decision _ diag@(UMLActivityDiagram _ conns) seen =
+  let ifElseEnd = head $ foldr1 intersect $ map (\x -> traverseFromNodes [x] diag [] seen) $ adjNodes decision diag
+      pathsToIfElseEnd = map (\xs -> filter (\x -> x `notElem` traverseFromNodes [ifElseEnd] diag [] seen) xs) 
+        $ map (\x -> traverseFromNodes [x] diag [] seen) $ adjNodes decision diag
+      subDiags = map (\xs -> UMLActivityDiagram xs conns) pathsToIfElseEnd
+      subStrings = map (\xs -> convertNode' [head $ nodes xs] xs seen) subDiags
+      newSeen = seen ++ (foldr1 union pathsToIfElseEnd) ++ [ifElseEnd]
+      newQueue = filter (`notElem` newSeen) (adjNodes ifElseEnd diag)
+  in (intercalate "forkagain\n" subStrings) ++ "forkend\n" ++ convertNode' newQueue diag newSeen
+
+
 --Strategy: Find the corresponding decision to the merge: That should be the node reachable from the merge (but not traversed yet) that has an edge towards it 
 --Then: Determine the subpath between the merge and the decision and handle them via convertNode'
 handleRepeat :: ADNode -> [ADNode] -> UMLActivityDiagram -> [ADNode] -> String
