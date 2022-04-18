c3914742070e27790dcec495da3d1b0b26757262
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-04-18 20:14:35 +0200

    Handler for decision nodes for plantumlconverter

--- src/AD_PlantUMLConverter.hs
+++ src/AD_PlantUMLConverter.hs
@@ -5,6 +5,7 @@ module AD_PlantUMLConverter (
   convertToPlantUML
 ) where
 
+import Data.List
 import Data.String.Interpolate ( i )
 
 import AD_Datatype (
@@ -23,8 +24,8 @@ convertToPlantUML diag =
 convertNode :: [ADNode] -> UMLActivityDiagram -> String
 convertNode queue diag = convertNode' queue diag []
 
---Use BFS to traverse the graph
---TODO: Handling fork/if-structures (finding a fork/decision node -> case distinction -> find corresponding merge -> traverse all paths to it)
+--Traverse the graph and serialize the nodes along the way to a PlantUML-String
+--TODO: Handling fork structures 
 convertNode' :: [ADNode] -> UMLActivityDiagram -> [ADNode] -> String
 convertNode' [] _ _ = ""
 convertNode' (current:queue) diag seen = 
@@ -34,26 +35,43 @@ convertNode' (current:queue) diag seen =
         ADActionNode {name} -> [i|:#{name};\n|] ++ convertNode' newQueue diag newSeen
         ADObjectNode {name} -> [i|:#{name}]\n|] ++ convertNode' newQueue diag newSeen
         ADInitialNode {} -> "start\n" ++ convertNode' newQueue diag newSeen
-        ADActivityFinalNode {} -> "end\n" ++ convertNode' newQueue diag newSeen
-        ADFlowFinalNode {} -> "stop\n" ++ convertNode' newQueue diag newSeen
-        ADMergeNode {} -> "repeat\n" ++ handleRepeat current convertNode' newQueue diag newSeen
+        ADActivityFinalNode {} -> "end\n" 
+        ADFlowFinalNode {} -> "stop\n" 
+        ADMergeNode {} -> "repeat\n" ++ handleRepeat current newQueue diag newSeen
+        ADDecisionNode {} -> "if () then ([X])\n" ++ handleDecision current newQueue diag newSeen
         _ -> ""
 
 
---Might be better to get repeat-structures directly from the alloy instance, but should work for now
---Traverses path until a decision node pointing towards the merge node is found, which is the corresponding repeatEnd
-handleRepeat :: ADNode -> ([ADNode] -> UMLActivityDiagram -> [ADNode] -> String) -> [ADNode] -> UMLActivityDiagram -> [ADNode] -> String
-handleRepeat _ _ [] _ _ = ""
-handleRepeat mergenode callback (current:queue) diag seen =
-    let newQueue = filter (`notElem` seen) (queue ++ adjNodes current diag)
-        newSeen = seen ++ [current]
-    in case current of 
-        ADActionNode {name} -> [i|:#{name};\n|] ++ handleRepeat mergenode callback newQueue diag newSeen
-        ADObjectNode {name} -> [i|:#{name}]\n|] ++ handleRepeat mergenode callback newQueue diag newSeen
-        ADInitialNode {} -> "start\n" ++ handleRepeat mergenode callback newQueue diag newSeen
-        ADActivityFinalNode {} -> "end\n" ++ handleRepeat mergenode callback newQueue diag newSeen
-        ADFlowFinalNode {} -> "stop\n" ++ handleRepeat mergenode callback newQueue diag newSeen
-        ADMergeNode {} -> "repeat\n" ++ handleRepeat current (handleRepeat mergenode callback) newQueue diag newSeen
-        ADDecisionNode {} -> if mergenode `elem` adjNodes current diag then "repeat while ()\n" ++ callback newQueue diag newSeen
-                             else "" -- TODO
-        _ -> "" 
+--Strategy: Find the corresponding merge to the decision: That should be the first node reachable from all decision paths
+--Since BFS returns nodes in order of their distance, the head of the intersection of the BFS-Path-Lists should be that node
+--Then: Determine the subpaths between the decision and the merge and handle them via convertNode'
+handleDecision :: ADNode -> [ADNode] -> UMLActivityDiagram -> [ADNode] -> String
+handleDecision decision _ diag@(UMLActivityDiagram _ conns) seen =
+  let ifElseEnd = head $ foldr1 intersect $ map (\x -> traverseFromNodes [x] diag [] seen) $ adjNodes decision diag
+      pathsToIfElseEnd = map (\xs -> takeWhile (\x -> x /= ifElseEnd) xs) $ map (\x -> traverseFromNodes [x] diag [] seen) $ adjNodes decision diag
+      subDiags = map (\xs -> UMLActivityDiagram xs conns) pathsToIfElseEnd
+      subStrings = map (\xs -> convertNode' [head $ nodes xs] xs seen) subDiags
+      newSeen = seen ++ (foldr1 union pathsToIfElseEnd) ++ [ifElseEnd]
+      newQueue = filter (`notElem` newSeen) (adjNodes ifElseEnd diag)
+  in (intercalate "else ([Y])\n" subStrings) ++ "endif\n" ++ convertNode' newQueue diag newSeen
+
+
+--Strategy: Find the corresponding decision to the merge: That should be the node reachable from the merge (but not traversed yet) that has an edge towards it 
+--Then: Determine the subpath between the merge and the decision and handle them via convertNode'
+handleRepeat :: ADNode -> [ADNode] -> UMLActivityDiagram -> [ADNode] -> String
+handleRepeat merge queue diag@(UMLActivityDiagram _ conns) seen =
+  let repeatEnd =  head $ dropWhile (\x -> merge `notElem` adjNodes x diag) $ traverseFromNodes [merge] diag [] seen
+      pathToRepeatEnd = tail $ takeWhile (\x -> merge `notElem` adjNodes x diag) $ traverseFromNodes [merge] diag [] seen
+      subString = convertNode' queue (UMLActivityDiagram pathToRepeatEnd conns) seen
+      newSeen = seen ++ pathToRepeatEnd ++ [repeatEnd] 
+      newQueue = filter (`notElem` newSeen) (adjNodes repeatEnd diag)
+  in subString ++ "repeat while ()\n" ++ convertNode' newQueue diag newSeen
+
+
+--Crude implementation of BFS, in order to get a list of reachable (yet unhandled) nodes 
+traverseFromNodes :: [ADNode] -> UMLActivityDiagram -> [ADNode] -> [ADNode] -> [ADNode]
+traverseFromNodes [] _ path _ = path
+traverseFromNodes (current:queue) diag path seen =
+  let newQueue = queue ++ filter (`notElem` (path ++ seen)) (adjNodes current diag)
+      newPath = path ++ [current]
+  in traverseFromNodes newQueue diag newPath seen 
