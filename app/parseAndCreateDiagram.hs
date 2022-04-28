b332462ee6092a67d5cfbfec42d9c8942b1cb2c4
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-04-28 19:47:24 +0200

    Multiple diagrams are now generatable at once

--- app/parseAndCreateDiagram.hs
+++ app/parseAndCreateDiagram.hs
@@ -3,6 +3,8 @@ module Main where
 import qualified Data.ByteString as B (writeFile)
 
 import System.Environment (getArgs, withArgs)
+import Control.Monad(sequence)
+
 
 import AD_Alloy (getAlloyInstances)
 import AD_Instance (parseInstance)
@@ -13,13 +15,13 @@ main :: IO ()
 main = do
   xs <- getArgs
   case xs of
-    pathToJar:pathToSvg:xs' -> do
-      inst <- getAlloyInstances (Just 1)
-      let ad = failWith id . parseInstance "this" "this" $ head inst
-          plantumlstring = convertToPlantUML ad
-      svg <- processPlantUMLString plantumlstring pathToJar
-      B.writeFile pathToSvg svg
-    _ -> error "usage: two parameters required: FilePath (PlantUML jar) FilePath (Output File)"
+    pathToJar:pathToFolder:xs' -> do
+      inst <- getAlloyInstances (Just 50) 
+      let ad = map (failWith id . parseInstance "this" "this") inst
+          plantumlstring = map convertToPlantUML ad
+      svg <- mapM (`processPlantUMLString` pathToJar) plantumlstring
+      mapM_ (\(x,y) -> B.writeFile (pathToFolder ++ "Diagram" ++ show x ++ ".svg") y) $ zip [1..] svg
+    _ -> error "usage: two parameters required: FilePath (PlantUML jar) FilePath (Output Folder)"
 
 failWith :: (a -> String) -> Either a c -> c
 failWith f = either (error . f) id
