db376331a6822bbc4315dc2e35a0aa5867c61458
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-05-17 20:24:41 +0200

    Function to generate at least one valid action seq

--- src/AD_ActionSequences.hs
+++ src/AD_ActionSequences.hs
@@ -1,21 +1,72 @@
 module AD_ActionSequences (
-  validActionSequence
+  validActionSequence,
+  generateActionSequence,
 ) where
 
-import Data.List (delete)
+import qualified Data.Set as S (fromList)
+import qualified Data.Map as M (filter, map, keys, toList, null)
 
 import AD_Datatype (
   ADNode(..),
-  UMLActivityDiagram,
-  adjNodes, getInitialNodes
+  UMLActivityDiagram (..),
+  adjNodes, getInitialNodes, isActionNode
   )
 
+import AD_Petrinet (
+  PetriKey(..),
+  convertToPetrinet 
+  )
+
+import Modelling.PetriNet.Types (
+  PetriLike(..),
+  Node(..),
+  isPlaceNode, isTransitionNode
+  )
+
+import Modelling.PetriNet.Reach.Type (
+  State(..),
+  Capacity(..),
+  Net(..)
+  )
+
+import Modelling.PetriNet.Reach.Step (levels')
+
+import Data.List (delete, find)
+
+
+fromPetriLike :: (Ord a) => PetriLike a -> Net a a
+fromPetriLike petri = 
+  Net {
+      places = S.fromList $ M.keys $ M.filter isPlaceNode $ allNodes petri,
+      transitions = S.fromList $ M.keys $ M.filter isTransitionNode $ allNodes petri,
+      connections = map (\(t,n) -> (M.keys $ flowIn n, t, M.keys $ flowOut n)) $ M.toList $ M.filter isTransitionNode $ allNodes petri,
+      capacity = Unbounded,
+      start = State {unState = M.map initial $ M.filter isPlaceNode $ allNodes petri}
+  }
+
+--Generate a single valid action sequence to one of the final nodes
+generateActionSequence :: UMLActivityDiagram -> Maybe [String]
+generateActionSequence diag =
+  case generateActionSequence' diag of
+    Just (t:_) -> Just $ map name $ filter (\n -> isActionNode n && (label :: ADNode -> Int) n `elem` (map (label :: PetriKey -> Int) t)) $ nodes diag
+    _ -> Nothing
+
+--Generate at least one sequence of transitions to a final node
+generateActionSequence' :: UMLActivityDiagram -> Maybe [[PetriKey]]
+generateActionSequence' diag =
+  let petriLike =  convertToPetrinet diag
+      finals = M.keys $ M.filter (M.null . flowOut) $ allNodes petriLike
+      sequences = find (not . null . filterSequences finals) $ levels' $ fromPetriLike petriLike
+  in (map reverse) <$> filterSequences finals <$> sequences
+  where 
+    p ts = \xs -> not . null $ filter (\t -> t `elem` xs) ts 
+    filterSequences ts = filter (p ts) . map snd
+
+
+
+--To be reworked
 validActionSequence :: [ADNode] -> UMLActivityDiagram -> Bool
 validActionSequence input diag = all isActionNode input && validActionSequence' input diag (getInitialNodes diag)
-
-isActionNode :: ADNode -> Bool 
-isActionNode (ADActionNode _ _) = True 
-isActionNode _ = False
 
 validActionSequence' :: [ADNode] -> UMLActivityDiagram -> [ADNode] -> Bool
 validActionSequence' [] _ [] = False
