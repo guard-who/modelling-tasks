ed6f273b6385cbfa92272e13e4bcc27938d0f514
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-05-19 20:45:52 +0200

    Unit Test for AD_ActionSequences

---
+++ test/AD_ActionSequencesSpec.hs
@@ -0,0 +1,167 @@
+module AD_ActionSequencesSpec where
+
+import AD_ActionSequences
+
+import AD_Datatype (
+  UMLActivityDiagram(..),
+  ADNode(..),
+  ADConnection(..)
+  )
+
+import Test.Hspec                          
+
+
+spec :: Spec 
+spec = do
+  describe "validActionSequence" $ do
+    it "accepts valid input sequences of actions, therefore one leading to the termination of all flows of the diagram" $
+      validActionSequence ["A", "E", "C", "B"] testDiagram `shouldBe` (True::Bool)
+    it "accepts valid input sequences traversing a different path at decision nodes" $
+      validActionSequence ["A", "E", "D", "B"] testDiagram `shouldBe` (True::Bool) 
+    it "accept valid input sequences traversing nodes in different order due to fork nodes" $
+      validActionSequence ["A", "B", "E", "C"] testDiagram `shouldBe` (True::Bool)   
+    it "accepts valid input sequences of actions traversing one cycle" $
+      validActionSequence ["A", "E", "C", "B", "D", "B", "E"] testDiagram `shouldBe` (True::Bool)
+    it "accepts valid input sequences of actions traversing more than one cycle" $
+      validActionSequence ["A", "E", "D", "B", "B", "C", "E", "E", "C", "B"] testDiagram `shouldBe` (True::Bool)
+    it "rejects an empty input sequence" $
+      validActionSequence [] testDiagram `shouldBe` (False::Bool)
+    it "rejects input sequences with actions which dont exist in the diagram" $
+      validActionSequence ["A", "X", "E", "C", "B"] testDiagram `shouldBe` (False::Bool)
+    it "rejects input sequences that are too short to terminate any flows of the diagram" $
+      validActionSequence ["A"] testDiagram `shouldBe` (False::Bool)
+    it "rejects input sequences that dont terminate all flows of the diagram" $
+      validActionSequence ["A", "E", "C"] testDiagram `shouldBe` (False::Bool)
+    it "rejects input sequences that contain actions that arent traversed in that order" $
+      validActionSequence ["A", "E", "A", "C", "B"] testDiagram `shouldBe` (False::Bool)
+    it "rejects input sequences where a prefix, but not the whole sequence, would terminate all flows of the diagram" $
+      validActionSequence ["A", "B", "D", "E", "B"] testDiagram `shouldBe` (False::Bool)
+    it "is consistent with the function generateActionSequence" $
+      validActionSequence (generateActionSequence testDiagram) testDiagram `shouldBe` (True::Bool)
+  
+
+testDiagram :: UMLActivityDiagram 
+testDiagram = UMLActivityDiagram 
+      { nodes =
+        [ ADActionNode
+            { label = 1
+            , name = "A"
+            }
+        , ADActionNode
+            { label = 2
+            , name = "B"
+            }
+        , ADActionNode
+            { label = 3
+            , name = "C"
+            }
+        , ADActionNode
+            { label = 4
+            , name = "D"
+            }
+        , ADActionNode
+            { label = 5
+            , name = "E"
+            }
+        , ADObjectNode
+            { label = 6
+            , name = "F"
+            }
+        , ADDecisionNode { label = 7 }
+        , ADDecisionNode { label = 8 }
+        , ADMergeNode { label = 9 }
+        , ADMergeNode { label = 10 }
+        , ADForkNode { label = 11 }
+        , ADJoinNode { label = 12 }
+        , ADFlowFinalNode { label = 13 }
+        , ADFlowFinalNode { label = 14 }
+        , ADInitialNode { label = 15 }
+        ]
+    , connections =
+        [ ADConnection
+            { from = 1
+            , to = 10
+            , guard = ""
+            }
+        , ADConnection
+            { from = 2
+            , to = 14
+            , guard = ""
+            }
+        , ADConnection
+            { from = 3
+            , to = 9
+            , guard = ""
+            }
+        , ADConnection
+            { from = 4
+            , to = 9
+            , guard = ""
+            }
+        , ADConnection
+            { from = 5
+            , to = 12
+            , guard = ""
+            }
+        , ADConnection
+            { from = 6
+            , to = 1
+            , guard = ""
+            }
+        , ADConnection
+            { from = 7
+            , to = 10
+            , guard = "b"
+            }
+        , ADConnection
+            { from = 7
+            , to = 13
+            , guard = "a"
+            }
+        , ADConnection
+            { from = 8
+            , to = 3
+            , guard = "b"
+            }
+        , ADConnection
+            { from = 8
+            , to = 4
+            , guard = "a"
+            }
+        , ADConnection
+            { from = 9
+            , to = 12
+            , guard = ""
+            }
+        , ADConnection
+            { from = 10
+            , to = 11
+            , guard = ""
+            }
+        , ADConnection
+            { from = 11
+            , to = 2
+            , guard = ""
+            }
+        , ADConnection
+            { from = 11
+            , to = 5
+            , guard = ""
+            }
+        , ADConnection
+            { from = 11
+            , to = 8
+            , guard = ""
+            }
+        , ADConnection
+            { from = 12
+            , to = 7
+            , guard = ""
+            }
+        , ADConnection
+            { from = 15
+            , to = 6
+            , guard = ""
+            }
+        ]
+    }
