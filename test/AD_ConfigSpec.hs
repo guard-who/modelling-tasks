d74a3490bbcab0a1352eb65ddb4a23a068c7e215
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-05-20 20:06:56 +0200

    Added test file for AD_Config module

---
+++ test/AD_ConfigSpec.hs
@@ -0,0 +1,15 @@
+module AD_ConfigSpec where
+
+import AD_Config (ADConfig(..), checkADConfig, defaultADConfig)
+
+import Test.Hspec (Spec, describe, it, context, shouldBe, shouldSatisfy)
+import Data.Maybe (isJust)
+
+spec :: Spec
+spec = describe "checkADConfig" $ do
+  it "checks if the basic Input is in given boundaries" $
+    checkADConfig defaultADConfig `shouldBe` Nothing
+  context "when provided with Input out of the constraints" $
+    it "it returns a String with nessecary changes" $
+      checkADConfig defaultADConfig{minActions=0, minObjectNodes=0} 
+        `shouldSatisfy` isJust
