e1e94aea3b5455ea41b9514fba1382c0278ae4e9
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-06-01 21:29:54 +0200

    Added module for task Identify the Components

---
+++ src/AD_MatchAD.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module AD_MatchAD (
+  MatchADInstance(..),
+  MatchADConfig(..),
+  MatchADSolution(..),
+  defaultMatchADConfig,
+  checkMatchADConfig,
+  matchADAlloy,
+  matchADTaskDescription,
+  matchADComponents,
+  matchADComponentsText
+ ) where
+
+
+import AD_Config (ADConfig(..), defaultADConfig, checkADConfig, adConfigToAlloy)
+import AD_Datatype (
+  UMLActivityDiagram(..),
+  ADNode(name),
+  isActionNode, isObjectNode, isDecisionNode, isMergeNode, isForkNode, isJoinNode, isActivityFinalNode, isFlowFinalNode)
+import AD_Shuffle (shuffleADNames)
+
+import Data.String.Interpolate ( i )
+
+
+data MatchADInstance = MatchADInstance {
+  activityDiagram :: UMLActivityDiagram,
+  seed :: Int
+} deriving (Show)
+
+newtype MatchADConfig = MatchADConfig {
+  adConfig :: ADConfig
+}
+
+defaultMatchADConfig :: MatchADConfig
+defaultMatchADConfig = MatchADConfig {
+  adConfig=defaultADConfig
+}
+
+checkMatchADConfig :: MatchADConfig -> Maybe String
+checkMatchADConfig MatchADConfig {
+  adConfig
+} = checkADConfig adConfig
+
+matchADAlloy :: MatchADConfig -> String
+matchADAlloy MatchADConfig {
+  adConfig
+}= adConfigToAlloy "" "" adConfig
+
+matchADTaskDescription :: String
+matchADTaskDescription =
+  [i|
+    Look at the given Activity Diagram, and use the displayed node names as identifiers
+    for the following tasks:
+
+    a) Name all Actions of the Activity Diagram
+    b) Name all Object Nodes of the Activity Diagram
+    c) Enter the number of Decision- and Merge Nodes in the Activity Diagram
+    d) Enter the number of Fork- and Join Nodes in the Activity Diagram
+    h) Enter the number of Activity Final Nodes in the Activity Diagram
+    i) Enter the number of Flow Final Nodes in the Activity Diagram
+  |]
+
+data MatchADSolution = MatchADSolution {
+  actionNames :: [String],
+  objectNodeNames :: [String],
+  numberOfDecisionMergeNodes :: Int,
+  numberOfForkJoinNodes :: Int,
+  numberOfActivityFinalNodes :: Int,
+  numberOfFlowFinalNodes :: Int
+} deriving (Eq, Show)
+
+matchADComponents :: MatchADInstance -> MatchADSolution
+matchADComponents MatchADInstance {
+  activityDiagram,
+  seed
+} =
+  let adnodes = nodes $ snd $ shuffleADNames seed activityDiagram
+  in MatchADSolution {
+      actionNames = map name $ filter isActionNode adnodes,
+      objectNodeNames = map name $ filter isObjectNode adnodes,
+      numberOfDecisionMergeNodes = length $ filter (\x -> isDecisionNode x || isMergeNode x) adnodes,
+      numberOfForkJoinNodes = length $ filter (\x -> isForkNode x || isJoinNode x) adnodes,
+      numberOfActivityFinalNodes = length $ filter isActivityFinalNode adnodes,
+      numberOfFlowFinalNodes = length $ filter isFlowFinalNode adnodes
+    }
+
+matchADComponentsText :: MatchADInstance -> String
+matchADComponentsText inst =
+  let solution = matchADComponents inst
+  in
+    [i|
+      Solutions for the MatchAD-Task:
+
+      a) Names of all Actions in the Activity Diagram: #{actionNames solution}
+      b) Names of all Object Nodes in the Activity Diagram: #{objectNodeNames solution}
+      c) Number of Decision- and Merge Nodes in the Activity Diagram: #{numberOfDecisionMergeNodes solution}
+      d) Number of Fork- and Join Nodes in the Activity Diagram: #{numberOfForkJoinNodes solution}
+      e) Number of Activity Final Nodes in the Activity Diagram: #{numberOfActivityFinalNodes solution}
+      f) Number of Flow Final Nodes in the Activity Diagram: #{numberOfFlowFinalNodes solution}
+    |]
