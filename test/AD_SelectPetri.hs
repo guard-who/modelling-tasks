2e4cd9cd5ac87fae3ebe1605cdfb3677084ba39b
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-06-06 23:35:32 +0200

    Added test for AD_SelectPetri module

---
+++ test/AD_SelectPetri.hs
@@ -0,0 +1,19 @@
+module AD_MatchComponentsSpec where
+
+import AD_SelectPetri (SelectPetriConfig(..), checkSelectPetriConfig, defaultSelectPetriConfig)
+
+import Test.Hspec (Spec, describe, it, context, shouldBe, shouldSatisfy)
+import Data.Maybe (isJust)
+import AD_Config (ADConfig(minActions, forkJoinPairs), defaultADConfig)
+
+
+spec :: Spec
+spec = do
+  describe "checkSelectPetriConfig" $ do
+    it "checks if the basic Input is in given boundaries" $
+      checkSelectPetriConfig defaultSelectPetriConfig  `shouldBe` Nothing
+    context "when provided with Input out of the constraints" $
+      it "it returns a String with necessary changes" $
+        checkSelectPetriConfig defaultSelectPetriConfig
+          {adConfig=defaultADConfig{minActions=0, forkJoinPairs=0}, avoidAddingSinksForFinals=Just True}
+            `shouldSatisfy` isJust
