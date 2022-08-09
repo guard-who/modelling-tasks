158ac806dbc3c1fa2cecbb2786fbb637072461ed
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-08-09 19:31:16 +0200

    Added test for petrinet conversion

---
+++ test/Modelling/ActivityDiagram/PetrinetSpec.hs
@@ -0,0 +1,37 @@
+module Modelling.ActivityDiagram.PetrinetSpec where
+
+import qualified Data.Map as M (keys)
+
+import Modelling.ActivityDiagram.Petrinet (PetriKey (..), ADNodeType(..), convertToPetrinet)
+
+import Modelling.ActivityDiagram.Alloy (getAlloyInstancesWith, moduleActionSequencesRules)
+import Modelling.ActivityDiagram.Config (adConfigToAlloy, defaultADConfig, ADConfig(..))
+import Modelling.ActivityDiagram.Instance (parseInstance)
+
+import Modelling.PetriNet.Types (PetriLike(allNodes), petriLikeToPetri)
+
+import Data.Either (isRight)
+import Data.List (sort)
+import Test.Hspec(Spec, context, describe, it, shouldBe)
+
+spec :: Spec
+spec =
+  describe "convertToPetrinet" $ do
+    context "on a list of generated diagrams" $ do
+      let spec = adConfigToAlloy "" "" defaultADConfig
+      it "generates a petrinet with ascending labels" $ do
+        inst <- getAlloyInstancesWith (Just 50) spec
+        let petri = map (convertToPetrinet . failWith id . parseInstance "this" "this") inst
+        all checkLabels petri `shouldBe` (True::Bool)
+      it "generates only valid petrinets" $ do
+        inst <- getAlloyInstancesWith (Just 50) spec
+        let petri = map (petriLikeToPetri . convertToPetrinet . failWith id . parseInstance "this" "this") inst
+        all isRight petri `shouldBe` (True::Bool)
+
+failWith :: (a -> String) -> Either a c -> c
+failWith f = either (error . f) id
+
+checkLabels :: PetriLike PetriKey -> Bool
+checkLabels petri =
+  let labels = sort $ map label $ M.keys $ allNodes petri
+  in labels == [1..(length labels)]
