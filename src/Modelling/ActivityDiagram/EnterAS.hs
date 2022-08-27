b99c326ca04b6ed80cac116ea5db71242c966c6c
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-08-27 18:35:35 +0200

    + default instance for EnterAS

--- src/Modelling/ActivityDiagram/EnterAS.hs
+++ src/Modelling/ActivityDiagram/EnterAS.hs
@@ -15,13 +15,14 @@ module Modelling.ActivityDiagram.EnterAS (
   enterASTask,
   enterASSyntax,
   enterASEvaluation,
-  enterAS
+  enterAS,
+  defaultEnterASInstance
 ) where
 
 import Modelling.ActivityDiagram.ActionSequences (generateActionSequence, validActionSequence)
 import Modelling.ActivityDiagram.Alloy (moduleActionSequencesRules)
 import Modelling.ActivityDiagram.Config (ADConfig(..), defaultADConfig, checkADConfig, adConfigToAlloy)
-import Modelling.ActivityDiagram.Datatype (UMLActivityDiagram(..), ADNode(..), isActionNode, isObjectNode)
+import Modelling.ActivityDiagram.Datatype (UMLActivityDiagram(..), ADNode(..), ADConnection(..), isActionNode, isObjectNode)
 import Modelling.ActivityDiagram.Instance (parseInstance)
 import Modelling.ActivityDiagram.PlantUMLConverter (drawADToFile, defaultPlantUMLConvConf)
 import Modelling.ActivityDiagram.Shuffle (shuffleADNames)
@@ -239,6 +240,48 @@ getEnterASTask config = do
   g' <- getRandom
   return $ EnterASInstance {activityDiagram=activityDiagram validInsta, seed=g'}
 
-
 failWith :: (a -> String) -> Either a c -> c
 failWith f = either (error . f) id
+
+defaultEnterASInstance :: EnterASInstance
+defaultEnterASInstance = EnterASInstance {
+  activityDiagram = UMLActivityDiagram
+      { nodes = [
+        ADActionNode { label = 1, name = "A"},
+        ADActionNode { label = 2, name = "B"},
+        ADActionNode { label = 3, name = "C"},
+        ADActionNode { label = 4, name = "D"},
+        ADActionNode { label = 5, name = "E"},
+        ADObjectNode { label = 6, name = "F"},
+        ADDecisionNode { label = 7 },
+        ADDecisionNode { label = 8 },
+        ADMergeNode { label = 9 },
+        ADMergeNode { label = 10 },
+        ADForkNode { label = 11 },
+        ADJoinNode { label = 12 },
+        ADFlowFinalNode { label = 13 },
+        ADFlowFinalNode { label = 14 },
+        ADInitialNode { label = 15 }
+      ]
+    , connections = [
+        ADConnection{ from = 1, to = 10, guard = ""},
+        ADConnection{ from = 2, to = 14, guard = ""},
+        ADConnection{ from = 3, to = 9, guard = ""},
+        ADConnection{ from = 4, to = 9, guard = ""},
+        ADConnection{ from = 5, to = 12, guard = ""},
+        ADConnection{ from = 6, to = 1, guard = ""},
+        ADConnection{ from = 7, to = 10, guard = "b"},
+        ADConnection{ from = 7, to = 13, guard = "a"},
+        ADConnection{ from = 8, to = 3, guard = "b"},
+        ADConnection{ from = 8, to = 4, guard = "a"},
+        ADConnection{ from = 9, to = 12, guard = ""},
+        ADConnection{ from = 10, to = 11, guard = ""},
+        ADConnection{ from = 11, to = 2, guard = ""},
+        ADConnection{ from = 11, to = 5, guard = ""},
+        ADConnection{ from = 11, to = 8, guard = ""},
+        ADConnection{ from = 12, to = 7, guard = ""},
+        ADConnection{ from = 15, to = 6, guard = ""}
+      ]
+    },
+    seed = 7777369639206507645
+}
