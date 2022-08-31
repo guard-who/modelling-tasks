42c1a9847c7f04ab5ec3d299b71cc6dc8e4dbcf9
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-09-01 00:00:52 +0200

    + task function, syntax checker and eval function for SelectPetri

--- src/Modelling/ActivityDiagram/SelectPetri.hs
+++ src/Modelling/ActivityDiagram/SelectPetri.hs
@@ -11,26 +11,48 @@ module Modelling.ActivityDiagram.SelectPetri (
   checkPetriInstance,
   selectPetriAlloy,
   selectPetrinet,
-  selectPetriTaskDescription
+  selectPetriTaskDescription,
+  selectPetriTask,
+  selectPetriSyntax,
+  selectPetriEvaluation
 ) where
+
+import qualified Data.Map as M (fromList, toList, keys, map, filter)
+import qualified Modelling.ActivityDiagram.Petrinet as PK (PetriKey(label))
 
 import Modelling.ActivityDiagram.Alloy (modulePetrinet)
 import Modelling.ActivityDiagram.Config (ADConfig(..), defaultADConfig, checkADConfig, adConfigToAlloy)
 import Modelling.ActivityDiagram.Datatype (UMLActivityDiagram(..), ADNode(..), isInitialNode, isActivityFinalNode, isFlowFinalNode)
 import Modelling.ActivityDiagram.Isomorphism (isPetriIsomorphic)
 import Modelling.ActivityDiagram.Petrinet (PetriKey(..), convertToPetrinet)
+import Modelling.ActivityDiagram.PlantUMLConverter (drawADToFile, defaultPlantUMLConvConf)
 import Modelling.ActivityDiagram.Shuffle (shuffleADNames)
 
 import Modelling.Auxiliary.Common (oneOf)
+import Modelling.Auxiliary.Output (addPretext)
+import Modelling.PetriNet.Diagram (cacheNet)
 import Modelling.PetriNet.Types (PetriLike(..))
 
 import Control.Applicative (Alternative ((<|>)))
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Control.Monad.Output (
+  LangM,
+  Rated,
+  OutputMonad (..),
+  english,
+  german,
+  translate,
+  translations,
+  singleChoice, singleChoiceSyntax
+  )
 import Control.Monad.Random (MonadRandom)
 import Data.List (unfoldr, nubBy)
+import Data.Map (Map)
 import Data.GraphViz.Commands (GraphvizCommand(Dot))
 import Data.String.Interpolate ( i )
 import System.Random (mkStdGen, next)       --To be changed from 'next' to 'uniform', not possible as of now due to dependencies
 import System.Random.Shuffle (shuffle')
+import Control.Monad.Except (runExceptT)
 
 
 data SelectPetriInstance = SelectPetriInstance {
@@ -172,3 +194,71 @@ swapST node =
 pickRandomItems :: Int -> [a] -> Int -> [a]
 pickRandomItems n xs seed =
   take n $ shuffle' xs (length xs) (mkStdGen seed)
+
+selectPetriTask
+  :: (OutputMonad m, MonadIO m)
+  => FilePath
+  -> SelectPetriInstance
+  -> LangM m
+selectPetriTask path task = do
+  let (diag, sol) = selectPetrinet task
+      mapping = M.map snd $ selectPetriSolutionToMap (seed task) sol
+  ad <- liftIO $ drawADToFile path defaultPlantUMLConvConf diag
+  paragraph $ translate $ do
+    english "Consider the following activity diagram."
+    german "Betrachten Sie das folgende Aktivitätsdiagramm."
+  image ad
+  petris <- liftIO $
+    traverse (\c -> runExceptT
+      $ cacheNet path (show . PK.label) c False False True Dot) mapping
+  paragraph $ translate $ do
+    english "Consider the following petrinets."
+    german "Betrachten Sie die folgenden Petrinetze."
+  images show id (M.map (failWith id) petris)
+  paragraph $ translate $ do
+    english [i|Which of these petrinets matches the given activity diagram?
+Please state your answer by giving a number indicating the matching petrinet.|]
+    german [i|Welcher dieser Petrinetze passt zum gegebenen Aktivitätsdiagramm?
+Bitte geben Sie ihre Antwort als Zahl an, welche das passende Petrinetz repräsentiert.|]
+  paragraph $ do
+    translate $ do
+      english [i|For example,|]
+      german [i|Zum Beispiel|]
+    code "2"
+    translate $ do
+      english [i|would indicate that petrinet 2 is the matching petrinet.|]
+      german  [i|würde bedeuten, dass Petrinetz 2 das passende Petrinetz ist.|]
+
+selectPetriSolutionToMap
+  :: Int
+  -> SelectPetriSolution
+  -> Map Int (Bool, PetriLike PetriKey)
+selectPetriSolutionToMap seed sol =
+  let xs = (True, matchingNet sol) : map (\x -> (False, x)) (wrongNets sol)
+      solution = shuffle' xs (length xs) (mkStdGen seed)
+  in M.fromList $ zip [1..] solution
+
+selectPetriSyntax
+  :: (OutputMonad m)
+  => SelectPetriInstance
+  -> Int
+  -> LangM m
+selectPetriSyntax task sub = addPretext $ do
+  let options = M.keys $ selectPetriSolutionToMap (seed task) $ snd $ selectPetrinet task
+  singleChoiceSyntax True options sub
+
+selectPetriEvaluation
+  :: OutputMonad m
+  => SelectPetriInstance
+  -> Int
+  -> Rated m
+selectPetriEvaluation task n = addPretext $ do
+  let as = translations $ do
+        english "petrinet"
+        german "Petrinet"
+      solMap = selectPetriSolutionToMap (seed task) $ snd $ selectPetrinet task
+      (solution, _) = head $ M.toList $ M.map snd $ M.filter fst solMap
+  singleChoice as (Just $ show solution) solution n
+
+failWith :: (a -> String) -> Either a c -> c
+failWith f = either (error . f) id
