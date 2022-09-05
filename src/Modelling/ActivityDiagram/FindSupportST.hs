fa808f89563e4a07e73093ce73b0d57b85f6dae9
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-09-05 15:50:42 +0200

    + default instance for FindSupportST

--- src/Modelling/ActivityDiagram/FindSupportST.hs
+++ src/Modelling/ActivityDiagram/FindSupportST.hs
@@ -13,12 +13,13 @@ module Modelling.ActivityDiagram.FindSupportST (
   findSupportSTText,
   findSupportSTTask,
   findSupportSTEvaluation,
-  findSupportST
+  findSupportST,
+  defaultFindSupportSTInstance
 ) where
 
 import qualified Data.Map as M ((!), null, size, filter, filterWithKey, keys, fromList)
 
-import Modelling.ActivityDiagram.Datatype (UMLActivityDiagram)
+import Modelling.ActivityDiagram.Datatype (UMLActivityDiagram(..), ADNode(..), ADConnection(..))
 import Modelling.ActivityDiagram.Petrinet (PetriKey(..), convertToPetrinet)
 import Modelling.ActivityDiagram.Shuffle (shuffleADNames)
 import Modelling.ActivityDiagram.Config (ADConfig(..), defaultADConfig, checkADConfig, adConfigToAlloy)
@@ -250,3 +251,50 @@ getFindSupportSTTask config = do
 
 failWith :: (a -> String) -> Either a c -> c
 failWith f = either (error . f) id
+
+defaultFindSupportSTInstance :: FindSupportSTInstance
+defaultFindSupportSTInstance = FindSupportSTInstance {
+  activityDiagram = UMLActivityDiagram {
+    nodes = [
+      ADActionNode {label = 1, name = "A"},
+      ADActionNode {label = 2, name = "B"},
+      ADActionNode {label = 3, name = "C"},
+      ADActionNode {label = 4, name = "D"},
+      ADObjectNode {label = 5, name = "E"},
+      ADObjectNode {label = 6, name = "F"},
+      ADObjectNode {label = 7, name = "G"},
+      ADObjectNode {label = 8, name = "H"},
+      ADDecisionNode {label = 9},
+      ADDecisionNode {label = 10},
+      ADMergeNode {label = 11},
+      ADMergeNode {label = 12},
+      ADForkNode {label = 13},
+      ADJoinNode {label = 14},
+      ADActivityFinalNode {label = 15},
+      ADFlowFinalNode {label = 16},
+      ADInitialNode {label = 17}
+    ],
+    connections = [
+      ADConnection {from = 1, to = 14, guard = ""},
+      ADConnection {from = 2, to = 11, guard = ""},
+      ADConnection {from = 3, to = 14, guard = ""},
+      ADConnection {from = 4, to = 9, guard = ""},
+      ADConnection {from = 5, to = 11, guard = ""},
+      ADConnection {from = 6, to = 10, guard = ""},
+      ADConnection {from = 7, to = 16, guard = ""},
+      ADConnection {from = 8, to = 4, guard = ""},
+      ADConnection {from = 9, to = 2, guard = "a"},
+      ADConnection {from = 9, to = 5, guard = "b"},
+      ADConnection {from = 10, to = 8, guard = "b"},
+      ADConnection {from = 10, to = 12, guard = "a"},
+      ADConnection {from = 11, to = 15, guard = ""},
+      ADConnection {from = 12, to = 6, guard = ""},
+      ADConnection {from = 13, to = 1, guard = ""},
+      ADConnection {from = 13, to = 3, guard = ""},
+      ADConnection {from = 13, to = 7, guard = ""},
+      ADConnection {from = 14, to = 12, guard = ""},
+      ADConnection {from = 17, to = 13, guard = ""}
+    ]
+  },
+  seed = 5508675034223564747
+}
