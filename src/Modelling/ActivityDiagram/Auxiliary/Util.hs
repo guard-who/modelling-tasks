ceb96a84a600547be710be6f3497170750a9fbd2
Athena Schulz <38783520+AthenaSchulz@users.noreply.github.com>
2022-09-28 21:22:28 +0200

    + function for weighted shuffle as utility (See fmidue/ba-athena-schulz#10)

--- src/Modelling/ActivityDiagram/Auxiliary/Util.hs
+++ src/Modelling/ActivityDiagram/Auxiliary/Util.hs
@@ -1,9 +1,16 @@
 module Modelling.ActivityDiagram.Auxiliary.Util (
   failWith,
   headEither,
-  headWithErr
+  headWithErr,
+  weightedShuffle
   ) where
 
+import Control.Monad.Random (
+  StdGen,
+  fromList,
+  runRand
+  )
+import Data.List (delete)
 import Data.Maybe (listToMaybe)
 
 failWith :: (a -> String) -> Either a c -> c
@@ -16,4 +23,20 @@ headEither err xs =
     Just x -> Right x
 
 headWithErr :: String -> [a] -> a
-headWithErr err = (failWith id) . headEither err
+headWithErr err = failWith id . headEither err
+
+{-
+  Shuffle a list of elements from type a based on given weights of type w,
+  where higher weight indicates a bigger probability of the element occuring
+  at a lower index of the list. The total weight of all elements must not be zero.
+-}
+weightedShuffle
+  :: (Eq a, Real w)
+  => StdGen
+  -> [(a,w)]
+  -> [a]
+weightedShuffle _ [] = []
+weightedShuffle g xs =
+  let rs = map (\x -> (x, toRational $ snd x)) xs
+      (a, g') = runRand (fromList rs) g
+  in fst a : weightedShuffle g' (delete a xs)
