8e26f1a2451236df1c3bcb63e5380ba32b111fe6
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2023-03-03 20:58:36 +0100

    add unit tests for object diagram drawing

--- test/Modelling/CdOd/OutputSpec.hs
+++ test/Modelling/CdOd/OutputSpec.hs
@@ -4,24 +4,34 @@ import Modelling.CdOd.CdAndChanges.Instance (
   GenericClassDiagramInstance (..),
   fromInstance,
   )
-import Modelling.CdOd.Output            (drawCd)
+import Modelling.CdOd.Output            (drawCd, drawOdFromInstance)
 import Modelling.Common                 (withUnitTests)
 
 import Control.Monad                    (void)
 import Control.Monad.Except             (runExceptT)
+import Control.Monad.Random             (evalRandT)
 import Data.ByteString.Char8            (pack)
+import Data.GraphViz                    (DirType (Back))
 import Test.Hspec
 import System.IO.Extra                  (withTempFile)
+import System.Random                    (mkStdGen)
 import Language.Alloy.Debug             (parseInstance)
 
 spec :: Spec
 spec = do
-  withUnitTests "drawCd" does dir "svg" $ shouldReturn . getResult
+  withUnitTests "drawCd" does dir "svg" $ shouldReturn . drawCdInstance
+  withUnitTests "drawOd" does dir "svg" $ shouldReturn . drawOdInstance
   where
     does = "draws expected class diagram"
     dir = "test/unit/Modelling/CdOd/Output"
-    getResult alloy = withTempFile $ \file -> do
+    drawCdInstance alloy = withTempFile $ \file -> do
       Right alloyInstance <- runExceptT $ parseInstance (pack alloy)
       Right cd <- return $ instanceClassDiagram <$> fromInstance alloyInstance
       void $ drawCd True True mempty cd file
+      readFile file
+    drawOdInstance alloy = withTempFile $ \file -> do
+      Right alloyInstance <- runExceptT $ parseInstance (pack alloy)
+      void $ flip evalRandT
+        (mkStdGen 0)
+        $ drawOdFromInstance alloyInstance (Just 1) Back True file
       readFile file
