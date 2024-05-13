7a37fbcd433f39a06c5accaf35ec6e246c215b3f
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2024-05-13 18:48:52 +0200

    add test cases for example configs

---
+++ example/test/ExamplesSpec.hs
@@ -0,0 +1,184 @@
+-- |
+
+module ExamplesSpec where
+
+import Modelling.CdOd.DifferentNames (
+  checkDifferentNamesConfig,
+  )
+import Modelling.CdOd.DifferentNames.Config (
+  task12,
+  task13,
+  task25,
+  )
+import Modelling.CdOd.MatchCdOd (
+  checkMatchCdOdConfig,
+  )
+import Modelling.CdOd.MatchCdOd.Config (
+  task14,
+  task15,
+  )
+import Modelling.CdOd.NameCdError (
+  checkNameCdErrorConfig,
+  )
+import Modelling.CdOd.NameCdError.Config (
+  task09,
+  task10,
+  )
+import Modelling.CdOd.RepairCd (
+  checkRepairCdConfig,
+  )
+import Modelling.CdOd.RepairCd.Config (
+  task07,
+  task08,
+  )
+import Modelling.CdOd.SelectValidCd (
+  checkSelectValidCdConfig,
+  )
+import Modelling.CdOd.SelectValidCd.Config (
+  task05,
+  task06,
+  )
+import Modelling.PetriNet.Concurrency (
+  checkFindConcurrencyConfig,
+  checkPickConcurrencyConfig,
+  )
+import Modelling.PetriNet.Conflict (
+  checkFindConflictConfig,
+  checkPickConflictConfig,
+  )
+import Modelling.PetriNet.MatchToMath (
+  checkMathConfig,
+  )
+import Modelling.PetriNet.PetriFindConcurrency.Config (
+  task23,
+  )
+import Modelling.PetriNet.PetriFindConflict.Config (
+  task24,
+  task26,
+  )
+import Modelling.PetriNet.PetriGraphToMath.Config (
+  task17,
+  task18,
+  )
+import Modelling.PetriNet.PetriMathToGraph.Config (
+  task19,
+  task20,
+  )
+import Modelling.PetriNet.PetriPickConcurrency.Config (
+  task21,
+  )
+import Modelling.PetriNet.PetriPickConflict.Config (
+  task22,
+  )
+import Modelling.ActivityDiagram.EnterAS (
+  checkEnterASConfig,
+  )
+import Modelling.ActivityDiagram.EnterAs.Config (
+  task35,
+  task36,
+  )
+import Modelling.ActivityDiagram.FindSupportST (
+  checkFindSupportSTConfig,
+  )
+import Modelling.ActivityDiagram.FindSupportSt.Config (
+  task41,
+  task42,
+  )
+import Modelling.ActivityDiagram.MatchAd (
+  checkMatchAdConfig,
+  )
+import Modelling.ActivityDiagram.MatchAd.Config (
+  task31,
+  task32,
+  )
+import Modelling.ActivityDiagram.MatchPetri (
+  checkMatchPetriConfig,
+  )
+import Modelling.ActivityDiagram.MatchPetri.Config (
+  task39,
+  task40,
+  )
+import Modelling.ActivityDiagram.SelectAS (
+  checkSelectASConfig,
+  )
+import Modelling.ActivityDiagram.SelectAs.Config (
+  task33,
+  task34,
+  )
+import Modelling.ActivityDiagram.SelectPetri (
+  checkSelectPetriConfig,
+  )
+import Modelling.ActivityDiagram.SelectPetri.Config (
+  task37,
+  task38,
+  )
+
+import Test.Hspec
+
+spec :: Spec
+spec =
+  describe "check config" $ do
+    it "task05" $
+      checkSelectValidCdConfig task05 `shouldBe` Nothing
+    it "task06" $
+      checkSelectValidCdConfig task06 `shouldBe` Nothing
+    it "task07" $
+      checkRepairCdConfig task07 `shouldBe` Nothing
+    it "task08" $
+      checkRepairCdConfig task08 `shouldBe` Nothing
+    it "task09" $
+      checkNameCdErrorConfig task09 `shouldBe` Nothing
+    it "task10" $
+      checkNameCdErrorConfig task10 `shouldBe` Nothing
+    it "task12" $
+      checkDifferentNamesConfig task12 `shouldBe` Nothing
+    it "task13" $
+      checkDifferentNamesConfig task13 `shouldBe` Nothing
+    it "task14" $
+      checkMatchCdOdConfig task14 `shouldBe` Nothing
+    it "task15" $
+      checkMatchCdOdConfig task15 `shouldBe` Nothing
+    it "task17" $
+      checkMathConfig task17 `shouldBe` Nothing
+    it "task18" $
+      checkMathConfig task18 `shouldBe` Nothing
+    it "task19" $
+      checkMathConfig task19 `shouldBe` Nothing
+    it "task20" $
+      checkMathConfig task20 `shouldBe` Nothing
+    it "task21" $
+      checkPickConcurrencyConfig task21 `shouldBe` Nothing
+    it "task22" $
+      checkPickConflictConfig task22 `shouldBe` Nothing
+    it "task23" $
+      checkFindConcurrencyConfig task23 `shouldBe` Nothing
+    it "task24" $
+      checkFindConflictConfig task24 `shouldBe` Nothing
+    it "task25" $
+      checkDifferentNamesConfig task25 `shouldBe` Nothing
+    it "task26" $
+      checkFindConflictConfig task26 `shouldBe` Nothing
+    it "task31" $
+      checkMatchAdConfig task31 `shouldBe` Nothing
+    it "task32" $
+      checkMatchAdConfig task32 `shouldBe` Nothing
+    it "task33" $
+      checkSelectASConfig task33 `shouldBe` Nothing
+    it "task34" $
+      checkSelectASConfig task34 `shouldBe` Nothing
+    it "task35" $
+      checkEnterASConfig task35 `shouldBe` Nothing
+    it "task36" $
+      checkEnterASConfig task36 `shouldBe` Nothing
+    it "task37" $
+      checkSelectPetriConfig task37 `shouldBe` Nothing
+    it "task38" $
+      checkSelectPetriConfig task38 `shouldBe` Nothing
+    it "task39" $
+      checkMatchPetriConfig task39 `shouldBe` Nothing
+    it "task40" $
+      checkMatchPetriConfig task40 `shouldBe` Nothing
+    it "task41" $
+      checkFindSupportSTConfig task41 `shouldBe` Nothing
+    it "task42" $
+      checkFindSupportSTConfig task42 `shouldBe` Nothing
