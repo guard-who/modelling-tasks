4fe9c8a008965f6097a04237b8cfff108c98eaf3
Marcellus Siegburg <marcellus.siegburg@uni-due.de>
2025-05-09 10:19:24 +0200

    add missing files (#285)

---
+++ src/Modelling/Auxiliary/Shuffle/All.hs
@@ -0,0 +1,78 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
+{-|
+Provides an interface to shuffle a 'taskInstance'
+by providing a 'ShuffleInstance' datatype.
+
+This module provides all means of shuffling.
+-}
+module Modelling.Auxiliary.Shuffle.All (
+  ShuffleInstance (..),
+  shuffleEverything,
+  shuffleInstance,
+  shuffleInstanceWith,
+  ) where
+
+import Modelling.Auxiliary.Common (
+  RandomiseNames (randomiseNames),
+  RandomiseLayout (randomiseLayout),
+  Randomise (randomise),
+  ShuffleExcept (unShuffleExcept),
+  )
+
+import Control.Exception                (SomeException)
+import Control.Monad                    ((>=>))
+import Control.Monad.Catch              (MonadThrow)
+import Control.Monad.Random             (MonadRandom, RandomGen, evalRandT)
+import GHC.Generics                     (Generic)
+
+-- | A datatype that allows setting all available shufflings
+data ShuffleInstance a = ShuffleInstance {
+  -- | The task instance to shuffle
+  taskInstance :: !a,
+  -- | If layout mangling should be permitted (affects graphics)
+  allowLayoutMangling :: !Bool,
+  -- | If names should be shuffled (affects component names)
+  shuffleNames :: !Bool,
+  -- | If available options should be shuffled (affects task description/answers)
+  shuffleOptions :: !Bool
+  } deriving (Eq, Generic, Read, Show)
+
+{-|
+Set all shuffling methods to enabled.
+-}
+shuffleEverything
+  :: (MonadRandom m, MonadThrow m, Randomise a, RandomiseLayout a, RandomiseNames a)
+  => a
+  -> m a
+shuffleEverything inst = shuffleInstance ShuffleInstance {
+  taskInstance = inst,
+  allowLayoutMangling = True,
+  shuffleNames = True,
+  shuffleOptions = True
+  }
+
+{-|
+Shuffle a 'taskInstance' based on enabled shuffling methods.
+-}
+shuffleInstance
+  :: (MonadRandom m, MonadThrow m, Randomise a, RandomiseLayout a, RandomiseNames a)
+  => ShuffleInstance a
+  -> m a
+shuffleInstance ShuffleInstance {..} =
+  whenM shuffleNames randomiseNames
+  >=> whenM shuffleOptions randomise
+  >=> whenM allowLayoutMangling randomiseLayout
+  $ taskInstance
+  where
+    whenM p x = if p then x else return
+
+{-|
+Shuffle a 'taskInstance' using 'shuffleInstance' and catch exceptions.
+-}
+shuffleInstanceWith
+  :: (RandomGen g, Randomise a, RandomiseLayout a, RandomiseNames a)
+  => ShuffleInstance a
+  -> g
+  -> Either SomeException a
+shuffleInstanceWith x = evalRandT (unShuffleExcept $ shuffleInstance x)
